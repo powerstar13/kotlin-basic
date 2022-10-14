14d13e8bdeb9097e26edf65fb7e2084908604ba5
홍준성 <ghdwnstjd13@gmail.com>
2022-10-14 11:12:16 +0900

    흐름제어 > if-else 표현식

---
+++ src/main/kotlin/03_If.kt
@@ -0,0 +1,27 @@
+fun main() {
+    // if-else 사용
+    val job = "Software Developer"
+
+    if (job == "Software Developer") {
+        println("개발자")
+    } else {
+        println("개발자 아님")
+    }
+
+    /**
+     * @see: Java_If.java
+     */
+    // 코틀린의 if-else는 표현식이다. (if-else 자체가 값을 return할 수 있기 때문에 이 자체를 표현식이라고 부른다.)
+    val age : Int = 10
+
+    val str = if (age > 10) {
+        "성인"
+    } else {
+        "아이"
+    }
+
+    // 코트린은 삼항 연산자가 없다. if-else가 표현식이므로 불필요하다.
+    val a = 1
+    val b = 2
+    val c = if (b > a) b else a
+}
